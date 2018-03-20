375bcb9b5ba90c9089ab2f1f50cd6a9aaff4ca28
Kuan-Ying <kuanying@usc.edu>
2018-03-19 17:45:06 -0700

    fixed untracked files

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://haloha1234:zero1234@ds257838.mlab.com:57838/leet-app",
-  cookieKey: 'fkljadfjaighfgogjpodfjasofjkapofjdidapjdpakfkogadjopf',
-  googleClientID: "796648150024-vb5jal66561o2ba7lbk5456op33id11h.apps.googleusercontent.com",
-  googleClientSecret: "liw8IfO--wHcxWVqABEDfyud"
-}
